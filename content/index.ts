f0ea7953d4e681f204c5fb864b5824fdc7a40690
Kang Shend Rong <shendrongddv@gmail.com>
2023-08-16 02:24:11 +0700

    20230815 - Categories

--- content/index.ts
+++ content/index.ts
@@ -70,3 +70,91 @@ export const contentPartners = [
     img: "Skill-Academy.png",
   },
 ];
+
+// Reviews
+export const contentReviews = [
+  {
+    id: 1,
+    name: "Agus Budiman",
+    gender: "man",
+    header: "Prakerja program sangat membantu saya",
+    body: "Saya sangat senang dengan program Prakerja. Program ini sangat membantu saya untuk meningkatkan keterampilan dan mendapatkan pekerjaan baru.",
+  },
+  {
+    id: 2,
+    name: "Susi Susanti",
+    gender: "woman",
+    header: "Prakerja program membuka banyak kesempatan",
+    body: "Prakerja program telah membuka banyak kesempatan bagi saya untuk belajar hal-hal baru dan mendapatkan keterampilan yang dibutuhkan di dunia kerja.",
+  },
+  {
+    id: 3,
+    name: "Rudi Widodo",
+    gender: "man",
+    header: "Prakerja program membuat saya lebih employable",
+    body: "Prakerja program telah membuat saya lebih employable dengan memberikan saya keterampilan yang dibutuhkan di dunia kerja.",
+  },
+  {
+    id: 4,
+    name: "Wulan Sari",
+    gender: "woman",
+    header: "Prakerja program adalah program yang sangat bermanfaat",
+    body: "Prakerja program adalah program yang sangat bermanfaat bagi masyarakat Indonesia. Program ini telah membantu banyak orang untuk meningkatkan keterampilan dan mendapatkan pekerjaan baru.",
+  },
+  {
+    id: 5,
+    name: "Budi Santoso",
+    gender: "man",
+    header: "Prakerja program adalah program yang tepat sasaran",
+    body: "Prakerja program adalah program yang tepat sasaran karena menargetkan masyarakat yang membutuhkan pekerjaan.",
+  },
+  {
+    id: 6,
+    name: "Ratna Sari Dewi",
+    gender: "woman",
+    header: "Prakerja program adalah program yang sangat diminati",
+    body: "Prakerja program adalah program yang sangat diminati oleh masyarakat Indonesia. Hal ini karena program ini memberikan banyak manfaat bagi masyarakat.",
+  },
+  {
+    id: 7,
+    name: "Doni Setiawan",
+    gender: "man",
+    header: "Prakerja program adalah program yang sangat terjangkau",
+    body: "Prakerja program adalah program yang sangat terjangkau bagi masyarakat Indonesia. Hal ini karena program ini dibiayai oleh pemerintah.",
+  },
+  {
+    id: 8,
+    name: "Nina Wijaya",
+    gender: "woman",
+    header: "Prakerja program adalah program yang sangat mudah diakses",
+    body: "Prakerja program adalah program yang sangat mudah diakses oleh masyarakat Indonesia. Hal ini karena program ini dapat diakses melalui website dan aplikasi.",
+  },
+  {
+    id: 9,
+    name: "Adi Prasetyo",
+    gender: "man",
+    header: "Prakerja program adalah program yang sangat inspiratif",
+    body: "Prakerja program adalah program yang sangat inspiratif bagi masyarakat Indonesia. Hal ini karena program ini telah membantu banyak orang untuk mengubah nasib mereka.",
+  },
+  {
+    id: 10,
+    name: "Ayu Lestari",
+    gender: "woman",
+    header: "Prakerja program adalah program yang sangat membanggakan",
+    body: "Prakerja program adalah program yang sangat membanggakan bagi Indonesia. Hal ini karena program ini telah membantu Indonesia untuk meningkatkan kualitas sumber daya manusianya.",
+  },
+  {
+    id: 11,
+    name: "Dian Ayu Lestari",
+    gender: "woman",
+    header: "Prakerja program adalah program yang sangat dihargai",
+    body: "Prakerja program adalah program yang sangat dihargai oleh masyarakat Indonesia. Hal ini karena program ini telah memberikan banyak manfaat bagi masyarakat.",
+  },
+  {
+    id: 12,
+    name: "Rama Adi Prasetyo",
+    gender: "man",
+    header: "Prakerja program adalah program yang sangat direkomendasikan",
+    body: "Prakerja program adalah program yang sangat direkomendasikan bagi masyarakat Indonesia yang ingin meningkatkan keterampilan dan mendapatkan pekerjaan baru.",
+  },
+];
