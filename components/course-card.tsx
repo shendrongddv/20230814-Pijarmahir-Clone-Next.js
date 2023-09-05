a62c438c1b718ea22389e993346318cbe5cbd103
Kang Shend Rong <shendrongddv@gmail.com>
2023-09-05 09:59:44 +0700

    20230905 : Course Card

--- components/course-card.tsx
+++ components/course-card.tsx
@@ -23,14 +23,14 @@ export const CourseCard = ({
   media,
 }: CourseCard) => {
   return (
-    <div className="w-[40vw] shrink-0 space-y-2 max-md:first:pl-4 max-md:last:pr-4 md:w-[30%]">
-      <div className="overflow-hidden rounded-xl">
+    <div className="w-[40vw] shrink-0 space-y-2 max-md:first:ml-4 max-md:last:mr-4 md:w-[30%]">
+      <div className="overflow-hidden rounded">
         <Image
           src={`/courses/${media}`}
           alt={title}
           width={343}
           height={179}
-          className="h-auto w-full"
+          className="h-auto w-full flex-1 shrink-0"
         />
       </div>
 
@@ -54,7 +54,7 @@ export const CourseCard = ({
           <Star className="h-3.5 w-3.5 pr-0.5 text-yellow-400 last:pr-0" />
         </div>
         <span className="ml-1 text-[10px] font-bold leading-none">5.0</span>
-        <span className="ml-2 text-[10px] leading-none text-muted-foreground">
+        <span className="ml-1 text-[10px] leading-none text-muted-foreground">
           ({reviewCount}&nbsp;Ulasan)
         </span>
       </div>
